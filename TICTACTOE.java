0f20b92a3578154ae0fc780a0246546834842ba0
Kumiko-IW <53671130+Kumiko-IW@users.noreply.github.com>
2023-01-04 01:23:58 +0900

    Update TICTACTOE.java
    ９回までの制限追加。比較メソッドのみ

--- TICTACTOE.java
+++ TICTACTOE.java
@@ -1,49 +1,42 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-public class tictactoe {
+public class Tictactoe {
 	public static void main(String args[]) {
 		//  ゲームボードに番地振ります
 		String[][] gameboard = {{"11","12","13"},{"21","22","23"},{"31","32","33"}};
 		String circle = "○";
 		String cross  = "×";
-		System.out.println(display_board);
-		//System.out.println("11|12|13|"); 
-		//System.out.println("21|22|23|");
-		//System.out.println("31|32|33|");
 		
-		//countOfInputが９になるか、game_endsフラグがTRUEになるまで○と×に交互に入力を促す
-		int countOfInput = 0;
+		//回数が９になるか、game_endsフラグがTRUEになるまで○と×に交互に入力を促す
 		boolean game_ends = false;
-		input_Number input_Circle_Number  = new input_Number;
-		input_Number input_Cross_Number = new input_Number;
-		
-		for(countOfInput < 9) {
-			while(!game_ends) {
-				countOfInput += 1;
-				//偶数なら”○”の番。それ以外は”×”の番
-				if(countOfInput％2 == 0) {
-					//入力受付メソッドの戻り値を受け取り、判定メソッドへ渡す
-					String circle_input_str = input_Circle_Number(gameboard);
-					//判定メソッドを呼び出し、勝敗がつけばgame_endsメソッドにTRUEを返す
-					game_ends = game_record(circle_input_str);
-					if(game_ends) {
-						break;
-				}else {
-					//入力受付メソッドの戻り値を受け取り、判定メソッドへ渡す
-					String cross_input_str = input_Cross_Number(gameboard);
-					//判定メソッドを呼び出し、勝敗がつけばgame_endsメソッドにTRUEを返す
-					game_ends = game_record(cross_input_str);
-					if(game_ends) {
-						break;
-				}//End Of If-else文
-			}//End Of while文
-			System.out.println("あいこです")
+		for(int i = 1; i <= 9; i++) {
+			//偶数なら”○”の番。それ以外は”×”の番
+			if(i % 2 == 0 ){
+				//入力受付メソッドの戻り値を受け取り、判定メソッドへ渡す
+				String circle_input_str = input_Number(circle, gameboard);
+				//判定メソッドを呼び出し、勝敗がつけばgame_endsメソッドにTRUEを返す
+				game_ends = game_record(circle_input_str);
+				if(game_ends) {
+					System.out.println("○の勝ちです");
+					break;
+				}//End Of break文
+			}else {
+				//入力受付メソッドの戻り値を受け取り、判定メソッドへ渡す
+				String cross_input_str = input_Number(cross, gameboard);
+				//判定メソッドを呼び出し、勝敗がつけばgame_endsメソッドにTRUEを返す
+				game_ends = game_record(cross_input_str);
+				if(game_ends) {
+					System.out.println("×の勝ちです");
+					break;
+				}//End Of break文
+			}//End Of If-else文
 		} //End Of for文
+		System.out.println("あいこです");
 	}   //　メインメソッドここまで	
 
 	//  入力受付と入力値バリデーションを行うメソッド。入力値を戻り値にする。
-	private static String input_Number(String gameboard[][]) {
+	private static String input_Number(String flag, String gameboard[][]) {
 		//  入力値の検証がtrue（空き番地として存在する２桁の数字のみ）になるまで再入力を促す
 		boolean valideted = false;
 		while(!valideted) {
@@ -53,7 +46,7 @@ public class tictactoe {
 			display_input_message(flag);
 			Scanner sc = new Scanner(System.in);
 			String input_Number    = sc.nextLine();
-			String input_arr[]  = input_str.split("");
+			String input_arr[]  = input_Number.split("");
 			//入力値のバリデーション＆アドレス空き確認のメソッド呼び出し
 			valideted = checkAddress(input_Number, input_arr,gameboard, valideted, flag);
 			if(valideted == true){
@@ -62,10 +55,6 @@ public class tictactoe {
 		}     // End Of while
 		return null;
 	}   // End Of turn
-
-	private static void display_input_message(String flag) {
-		System.out.println(flag + "を書く場所を数字2桁で入力してください");
-	}
 
 	//gameboadを表示させるメソッド
 	private static void display_board(String gameboard[][]) {
@@ -86,8 +75,12 @@ public class tictactoe {
 		}//End Of iループ
 	}//End Of showboad
 	
+	static void display_input_message(String flag) {
+		System.out.println(flag + "を書く場所を数字2桁で入力してください");
+	}
+	
 	//入力値のバリデーション＆アドレスの空き確認メソッド
-	private static boolean checkAddress(String input_str, String input_arr[], String gameboard[][], boolean valideted ,String flag) {
+	static boolean checkAddress(String input_str, String input_arr[], String gameboard[][], boolean valideted ,String flag) {
 		//入力値が２桁以外ならFALSEを返す。
 		if(input_str.length() != 2) {
 			System.out.println("アドレスは２桁で入力してください。");
@@ -136,4 +129,3 @@ public class tictactoe {
 		return false;
 	}//End Of game_recordメソッド
 }//End Of Class
-
